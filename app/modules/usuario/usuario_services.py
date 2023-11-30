28f0a213a917d45f16391d538076fae5f25efea0
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2023-11-30 18:52:49 -0500

    Tipado agregado a usuario services

--- app/modules/usuario/usuario_services.py
+++ app/modules/usuario/usuario_services.py
@@ -1,5 +1,6 @@
 from app.modules.usuario.usuario_database_model import Usuario
+from sqlalchemy.orm import Session
 
 
-def obtener_usuario_por_id(user_id: int, db):
+def obtener_usuario_por_id(user_id: int, db: Session):
     return db.query(Usuario).filter(Usuario.id == user_id).first()
