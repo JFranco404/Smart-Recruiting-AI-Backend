b736c1fed58a8d5cddd877b5e3e8b1ab72cbdedf
danearboleda <nataarboleda2001@gmail.com>
2023-12-02 15:57:08 -0500

    Crear y actualizar perfil postulante

--- app/modules/perfil_postulante/perfil_postulante_routes.py
+++ app/modules/perfil_postulante/perfil_postulante_routes.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter, HTTPException
 from sqlalchemy.orm import Session
 from fastapi import Depends
 from database.db import get_db
-from app.modules.perfil_postulante.perfil_postulante_services import obtener_perfil_postulante_por_usuario_id
+from app.modules.perfil_postulante.perfil_postulante_services import *
+from app.modules.perfil_postulante.perfil_postulante_models import DatosPostulante
 
 router = APIRouter()
 
@@ -16,3 +17,22 @@ async def ruta_obtener_perfil_postulante_por_usuario_id(usuario_id: int, db: Ses
 
     except ValueError as error:
         return HTTPException(status_code=404, detail=str(error))
+
+@router.post('/perfil-postulante', tags=["Perfil Postulante"])
+async def ruta_crear_perfil_postulante(postulante:DatosPostulante, db:Session= Depends(get_db)):
+    try:
+        postulante_creado = crear_postulante(postulante, db)
+        return postulante_creado
+
+    except ValueError as error:
+        return HTTPException(status_code=400, detail=str(error))
+    
+@router.put('/perfil-postulante', tags=["Perfil Postulante"])
+async def ruta_actualizar_perfil_postulante(postulante:ActualizarPerfilPostulante, db:Session= Depends(get_db)):
+    try:
+        postulante_actualizado = actualizar_postulante(postulante, db)
+        return postulante_actualizado
+
+    except ValueError as error:
+        return HTTPException(status_code=400, detail=str(error))
+    
--- app/modules/perfil_postulante/perfil_postulante_services.py
+++ app/modules/perfil_postulante/perfil_postulante_services.py
@@ -2,6 +2,7 @@ from typing import List
 from sqlalchemy.orm import Session
 from app.modules.perfil_postulante.perfil_postulante_database_model import PerfilPostulante
 from app.modules.usuario.usuario_services import obtener_usuario_por_id
+from app.modules.perfil_postulante.perfil_postulante_models import DatosPostulante, ActualizarPerfilPostulante
 
 
 def obtener_perfil_postulante_por_usuario_id(usuario_id: int, db: Session) -> PerfilPostulante:
@@ -10,8 +11,47 @@ def obtener_perfil_postulante_por_usuario_id(usuario_id: int, db: Session) -> Pe
         raise ValueError("El usuario no existe")
 
     perfil_postulante = db.query(PerfilPostulante).filter(
-        PerfilPostulante.id_usuario == usuario_id)
+        PerfilPostulante.id_usuario == usuario_id).first()
     if not perfil_postulante:
         raise ValueError("El perfil no existe")
 
     return perfil_postulante
+
+
+def crear_postulante(postulante: DatosPostulante, db:Session) -> PerfilPostulante:
+    usuario_id = obtener_usuario_por_id(postulante.id_usuario,db)
+    if not usuario_id:
+        raise ValueError("El usuario no existe")
+
+    postulante = PerfilPostulante(
+        id_usuario=postulante.id_usuario,
+        resumen=postulante.resumen,
+        habilidades=postulante.habilidades,
+        idiomas=postulante.idiomas,
+        link=postulante.link,
+        referencias=postulante.referencias
+    )
+
+    #db.add(postulante)
+    #db.commit()
+    #db.refresh(postulante)
+
+    return postulante
+
+def actualizar_postulante(postulante: ActualizarPerfilPostulante, db:Session) ->PerfilPostulante:
+    postulante_encontrado = db.query(PerfilPostulante).filter(
+        PerfilPostulante.id == postulante.id).first()
+    if not postulante_encontrado:
+        raise ValueError("El perfil del postulante no existe")
+    
+    postulante_encontrado.id_usuario=postulante.id_usuario
+    postulante_encontrado.resumen=postulante.resumen
+    postulante_encontrado.habilidades=postulante.habilidades
+    postulante_encontrado.idiomas=postulante.idiomas
+    postulante_encontrado.link=postulante.link
+    postulante_encontrado.referencias=postulante.referencias
+
+    db.commit()
+    db.refresh(postulante_encontrado)
+
+    return postulante_encontrado
