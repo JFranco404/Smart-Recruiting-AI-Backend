29ebe3c9702669a387d63ba5d36f2964c9616466
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2023-12-03 15:02:10 -0500

    Se corrige etiqueta en modulo auth

--- app/modules/auth/auth_routes.py
+++ app/modules/auth/auth_routes.py
@@ -6,7 +6,8 @@ from app.modules.auth.auth_services import *
 
 router = APIRouter()
 
-@router.post("/auth/login", tags="[Auth]")
+
+@router.post("/auth/login", tags=["Auth"])
 def login(auth: Auth_login, db: Session = Depends(get_db)):
     try:
         token = login_service(auth, db)
@@ -14,7 +15,8 @@ def login(auth: Auth_login, db: Session = Depends(get_db)):
     except ValueError as error:
         return HTTPException(status_code=404, detail=str(error))
 
-@router.post("/auth/registrarse", tags="[Auth]")
+
+@router.post("/auth/registrarse", tags=["Auth"])
 def register(auth: Auth_register, db: Session = Depends(get_db)):
     try:
         return registrar_usuario(auth, db)
