4e5fb1391c5720d5e7f8ebe96c599238466c154b
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2023-12-03 15:03:13 -0500

    Ruta agregada para modulo de hoja de vida

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from app.modules.vacante import vacante_routes
 from app.modules.perfil_postulante import perfil_postulante_routes
 from app.modules.postulacion import postulacion_routes
 from app.modules.auth import auth_routes
+from app.modules.hoja_de_vida import hoja_de_vida_routes
 
 def include_routes(app):
     app.include_router(main_routes.router, tags=["Principal"])
@@ -10,3 +11,4 @@ def include_routes(app):
     app.include_router(perfil_postulante_routes.router, tags=["Perfil Postulante"])
     app.include_router(postulacion_routes.router, tags=["Postulation"])
     app.include_router(auth_routes.router, tags=["Auth"])
+    app.include_router(hoja_de_vida_routes.router, tags=["Hoja de vida"])
