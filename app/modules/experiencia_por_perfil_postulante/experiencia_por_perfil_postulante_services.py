8306e5d1e9805f18ea096ee415aac5c8c4d24529
danearboleda <nataarboleda2001@gmail.com>
2023-12-04 10:39:06 -0500

    Ajustes finales tablas intermedias educacion y experiencia

--- app/modules/experiencia_por_perfil_postulante/experiencia_por_perfil_postulante_services.py
+++ app/modules/experiencia_por_perfil_postulante/experiencia_por_perfil_postulante_services.py
@@ -4,11 +4,21 @@ from app.modules.historial_academico.historial_academico_services import obtener
 from app.modules.perfil_postulante.perfil_postulante_services import obtener_perfil_postulante_por_usuario_id
 from app.modules.experiencia_por_perfil_postulante.experiencia_por_perfil_postulante_database_model import ExperienciaPorPerfilPostulante
 from app.modules.educacion_por_perfil_postulante.educacion_por_perfil_postulante_services import obtener_perfil_postulante_por_usuario_id
+from app.modules.experiencia.experiencia_services import obtener_experiencia_por_id
 
 def obtener_experiencia_por_perfil_postulante_por_postulante_id(postulante_id:int, db: Session) -> List[ExperienciaPorPerfilPostulante]:
     postulante = obtener_perfil_postulante_por_usuario_id(postulante_id, db)
     if not postulante:
         raise ValueError("El usuario reclutador no existe")
 
-    return db.query(ExperienciaPorPerfilPostulante).filter(
+    experiencias_por_perfil = db.query(ExperienciaPorPerfilPostulante).filter(
         ExperienciaPorPerfilPostulante.id_perfil_postulante == postulante_id).all()
+    
+    experiencias = []
+    for i in experiencias_por_perfil:
+        experiencias.append(obtener_experiencia_por_id(i.id, db))
+
+    return experiencias
+
+
+
