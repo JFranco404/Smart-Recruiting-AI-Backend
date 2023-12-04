e2db3efe31462d7b6be1004ca5da718c2777758f
samyquito02 <samuel.1702012523@ucaldas.edu.co>
2023-12-03 19:27:08 -0500

    se genera hoja de vida con información básica

--- app/modules/hoja_de_vida/hoja_de_vida_services.py
+++ app/modules/hoja_de_vida/hoja_de_vida_services.py
@@ -1,6 +1,8 @@
 from sqlalchemy.orm import Session
 from app.shared.generar_archivo.generar_archivo import FabricaDeArchivos
 from app.shared.generar_archivo.tipo_plantilla import TipoPlantilla
+from app.modules.usuario.usuario_services import obtener_usuario_por_id
+from app.modules.perfil_postulante.perfil_postulante_services import obtener_perfil_postulante_por_usuario_id
 
 
 def generar_hoja_de_vida(id_usuario: int, db: Session) -> str:
@@ -11,9 +13,17 @@ def generar_hoja_de_vida(id_usuario: int, db: Session) -> str:
 
 def __obtener_datos_hoja_de_vida(id_usuario: int, db: Session) -> dict:
     '''Obtiene los datos necesarios para generar una hoja de vida'''
+    usuario = obtener_usuario_por_id(id_usuario,db)
+    postulante = obtener_perfil_postulante_por_usuario_id(id_usuario,db)
     return {
-        "usuario": "Juan",
-        "apellido": "Perez",
-        "edad": 20,
-        "correo": "juan.perez@gmail.com",
+        "nombres": usuario.nombres,
+        "apellidos": usuario.apellidos,
+        "correo": usuario.correo,
+        "lugar_residencia": usuario.lugar_residencia,
+        "resumen": postulante.resumen,
+        "habilidades": postulante.habilidades,
+        "idiomas": postulante.idiomas,
+        "link": postulante.link,
+        "referencias": postulante.referencias,
+
     }
--- app/shared/generar_archivo/generadores_html/generador_html_hoja_de_vida.py
+++ app/shared/generar_archivo/generadores_html/generador_html_hoja_de_vida.py
@@ -1,6 +1,79 @@
 def generar_html_hoja_de_vida(datos_hoja_de_vida: dict):
+    estilos = obtener_estilos()
     return f"""
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hoja de Vida</title>
+    <style>
+       {estilos} 
+    </style>
+    </head>
     <body>
-        <h1>Hoja de vida {datos_hoja_de_vida["usuario"]}</h1>
+
+        <header>
+        <h1>Hoja de vida {datos_hoja_de_vida["nombres"]}</h1>
+        </header>
+
+        <section>
+        <h2>Información Personal</h2>
+        <ul>
+            <li><strong>Nombre:</strong> {datos_hoja_de_vida["nombres"]}</li>
+            <li><strong>Lugar de Residencia:</strong> {datos_hoja_de_vida["lugar_residencia"]}</li>
+            <li><strong>Correo:</strong> {datos_hoja_de_vida["correo"]}</li>
+        </ul>
+        </section>
+
+        <section>
+            <h2>Perfil Postulante</h2>
+            <ul>
+                <li><strong>Resumen:</strong> {datos_hoja_de_vida["resumen"]}</li>
+                <li><strong>Habilidades:</strong> {datos_hoja_de_vida["habilidades"]}</li>
+                <li><strong>Idiomas:</strong> {datos_hoja_de_vida["idiomas"]}</li>
+                <li><strong>Enlace:</strong> {datos_hoja_de_vida["link"]}</li>
+                <li><strong>Referencias:</strong> {datos_hoja_de_vida["referencias"]}</li>
+            </ul>
+        </section>
     </body>
     """
+
+def obtener_estilos():
+    return """
+    body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+
+        h1 {
+            text-align: center;
+        }
+
+        section {
+            margin-bottom: 20px;
+        }
+
+        h2 {
+            border-bottom: 1px solid #ccc;
+            padding-bottom: 5px;
+        }
+
+        p {
+            margin: 0;
+        }
+
+        ul {
+            list-style-type: none;
+            padding: 0;
+        }
+
+        li {
+            margin-bottom: 5px;
+        }
+
+        a {
+            color: #007BFF;
+        }
+    """
+
+
